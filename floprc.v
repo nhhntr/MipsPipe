8868bab09eedd0324daf6650b84b617b18d96960
nhhntr <nhhntr11@gmail.com>
2014-05-01 01:05:28 -0500

    Delete floprc.v

--- floprc.v
+++
@@ -1,9 +0,0 @@
-module floprc #(parameter WIDTH = 8)
-					(input clk, reset, clear, input [WIDTH-1:0] d, output reg [WIDTH-1:0] q);
-
-	always @(posedge clk, posedge reset)
-		if (reset) q <= #1 0;
-		else if (clear) q <= #1 0;
-			  else q <= #1 d;
-
-endmodule
